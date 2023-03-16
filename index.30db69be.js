b9ff60842c6789ae2b6f86848226eea5e5c4a831
niver-nieq <118331060+niver-nieq@users.noreply.github.com>
2023-03-16 21:47:34 +0000

    Deploying to gh-pages from @ niver-nieq/trabajo-final-html@342351d34e4824b3037c35092944234185101aa3 🚀

---
+++ index.30db69be.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-1-open]"),closeModalBtn:document.querySelector("[data-modal-close-1]"),modal:document.querySelector("[data-modal-1]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.30db69be.js.map
